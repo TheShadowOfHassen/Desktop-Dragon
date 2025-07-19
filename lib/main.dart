650539636426e0a4f7c8f24b59a2b45663b73472
TheShadowOfHassen <94244235+TheShadowOfHassen@users.noreply.github.com>
2025-07-19 18:33:13 -0400

    tryp1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:date_format/date_format.dart';
+import 'dart:async';
 
 void main() => runApp(DesktopDragon());
 
@@ -12,6 +13,7 @@ var buttonTwoWebsites = [
   'https://www.gamingonlinux.com/',
   'https://itsfoss.com/',
 ];
+var label_time = 'dd';
 
 var buttonThree = 'MUSIC';
 var buttonThreeWebsites = ['https://open.spotify.com/'];
@@ -24,6 +26,26 @@ class DesktopDragon extends StatefulWidget {
 }
 
 class _DesktopDragonState extends State<DesktopDragon> {
+  String labelTime = '';
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    // Update every second
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        labelTime = updateClock();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       // Root widget
@@ -80,33 +102,31 @@ class _DesktopDragonState extends State<DesktopDragon> {
     );
   }
 
-  var label_time = updateClock();
-}
-
-void open_urls(List urls) {
-  for (var name in urls) {
-    Uri url = Uri.parse(name);
-    launchUrl(url);
+  void open_urls(List urls) {
+    for (var name in urls) {
+      Uri url = Uri.parse(name);
+      launchUrl(url);
+    }
   }
-}
 
-String updateClock() {
-  var time = formatDate(DateTime.now(), [
-    DD,
-    ' ',
-    MM,
-    ' ',
-    dd,
-    ', ',
-    yyyy,
-    '\n',
-    h,
-    ':',
-    n,
-    ':',
-    s,
-    '  ',
-    am,
-  ]);
-  return time;
+  String updateClock() {
+    var time = formatDate(DateTime.now(), [
+      DD,
+      ' ',
+      MM,
+      ' ',
+      dd,
+      ', ',
+      yyyy,
+      '\n',
+      h,
+      ':',
+      n,
+      ':',
+      s,
+      '  ',
+      am,
+    ]);
+    return time;
+  }
 }
