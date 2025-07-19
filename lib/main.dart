f3818c2025f2fd8a560d5da1716b5a8a3d8494f2
TheShadowOfHassen <94244235+TheShadowOfHassen@users.noreply.github.com>
2025-07-19 12:04:02 -0400

    more set up

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Builder(
+            builder: (context) {
+              return Column(
+                children: [
+                  const Text(
+                    '  ^^     /======>\n (00)   /      /\n@@=====>\n    J        J',
+                   ),
+                ];
+              );  
+            }
         ),
       ),
     );
