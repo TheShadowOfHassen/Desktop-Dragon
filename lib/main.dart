07a4d418470950346bdbba69accb8c322cce609b
TheShadowOfHassen <94244235+TheShadowOfHassen@users.noreply.github.com>
2025-07-20 18:57:15 -0400

    setupstuff

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'dart:async';
 import 'package:window_manager/window_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 const SIZE = Size(400, 400);
 
@@ -16,8 +17,45 @@ void main() async {
   WindowManager.instance.setMinimumSize(SIZE);
   WindowManager.instance.setMaximumSize(SIZE);
   WindowManager.instance.setSize(SIZE);
-  runApp(DesktopDragon());
+  runApp(
+    MaterialApp(
+      title: 'Desktop Dragon',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 3, 3, 3),
+          brightness: Brightness.dark,
+        ),
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(fontSize: 72),
+          // ···
+          titleLarge: GoogleFonts.getFont('IBM Plex Mono'),
+          bodyMedium: GoogleFonts.getFont('IBM Plex Mono'),
+          displaySmall: GoogleFonts.getFont('IBM Plex Mono'),
+        ),
+      ),
+      home: DesktopDragon(),
+    ),
+  );
 }
+
+var data = [
+  [
+    'Button One',
+    ['https://github.com/'],
+  ],
+  [
+    'Button Two',
+    ['https://www.gamingonlinux.com/'],
+  ],
+  [
+    'Music',
+    ['open.spotify.com'],
+  ],
+];
 
 var buttonOne = 'Button One';
 var buttonOneWebsites = ['https://github.com/'];
@@ -39,6 +77,12 @@ class DesktopDragon extends StatefulWidget {
 
 class _DesktopDragonState extends State<DesktopDragon> {
   String labelTime = '';
+  String ButtonOneLabel = '';
+  List ButtonOneList = [];
+  String ButtonTwoLabel = '';
+  List ButtonTwoList = [];
+  String ButtonThreeLabel = '';
+  List ButtonThreeList = [];
   Timer? _timer;
 
   @override
@@ -59,93 +103,73 @@ class _DesktopDragonState extends State<DesktopDragon> {
   }
 
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Desktop Dragon',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // Define the default brightness and colors.
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 3, 3, 3),
-          // ···
-          brightness: Brightness.dark,
-        ),
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: TextTheme(
-          displayLarge: const TextStyle(fontSize: 72),
-          // ···
-          titleLarge: GoogleFonts.getFont('IBM Plex Mono'),
-          bodyMedium: GoogleFonts.getFont('IBM Plex Mono'),
-          displaySmall: GoogleFonts.getFont('IBM Plex Mono'),
-        ),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Desktop Dragon'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.settings),
-              tooltip: 'Show Settings',
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: const Icon(Icons.close),
-              tooltip: 'Close The App',
-              onPressed: () {
-                //save the app
-                SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-              },
-            ),
-          ],
-        ),
-
-        body: Center(
-          child: Builder(
-            builder: (context) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(labelTime),
-                      const SizedBox(width: 50),
-                      Text(
-                        '  ^^     /=====/\n (00)   /     /\n@@=============\\\n    J      J   V',
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          open_urls(buttonOneWebsites);
-                        },
-                        child: Text(buttonOne),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          open_urls(buttonTwoWebsites);
-                        },
-                        child: Text(buttonTwo),
-                      ),
-
-                      ElevatedButton(
-                        onPressed: () {
-                          open_urls(buttonThreeWebsites);
-                        },
-                        child: Text(buttonThree),
-                      ),
-                    ],
-                  ),
-                ],
-              );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Desktop Dragon'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings),
+            tooltip: 'Show Settings',
+            onPressed: () {
+              _showMyDialog(context);
             },
           ),
+          IconButton(
+            icon: const Icon(Icons.close),
+            tooltip: 'Close The App',
+            onPressed: () {
+              //save the app
+              SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+            },
+          ),
+        ],
+      ),
+
+      body: Center(
+        child: Builder(
+          builder: (context) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(labelTime),
+                    const SizedBox(width: 50),
+                    Text(
+                      '  ^^     /=====/\n (00)   /     /\n@@=============\\\n    J      J   V',
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        open_urls(ButtonOneList);
+                      },
+                      child: Text(ButtonOneLabel),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        open_urls(ButtonTwoList);
+                      },
+                      child: Text(ButtonTwoLabel),
+                    ),
+
+                    ElevatedButton(
+                      onPressed: () {
+                        open_urls(ButtonThreeList);
+                      },
+                      child: Text(ButtonThreeLabel),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
@@ -178,4 +202,80 @@ class _DesktopDragonState extends State<DesktopDragon> {
     ]);
     return time;
   }
+}
+
+Future<List> getData() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  // For the first button
+  String? button_oneLabel = prefs.getString('button_oneLabel');
+  if (button_oneLabel == null) {
+    button_oneLabel = 'Button One';
+  }
+  ;
+  List<String>? button_oneList = prefs.getStringList('button_oneList');
+  if (button_oneList == null) {
+    var button_oneList = <String>['github.com'];
+  }
+  ;
+  // For the second button
+  String? button_twoLabel = prefs.getString('button_twoLabel');
+  if (button_twoLabel == null) {
+    button_twoLabel = 'Button Two';
+  }
+  ;
+  List<String>? button_twoList = prefs.getStringList('button_twoList');
+  if (button_twoList == null) {
+    var button_twoList = <String>[
+      'https://ubuntu-flutter-community.github.io/website/',
+    ];
+  }
+  ;
+  // For the third button
+  String? button_threeLabel = prefs.getString('button_threeLabel');
+  if (button_threeLabel == null) {
+    button_threeLabel = 'Button Three';
+  }
+  ;
+  List<String>? button_threeList = prefs.getStringList('button_threeList');
+  if (button_threeList == null) {
+    button_threeList = <String>['https://github.com/TheShadowOfHassen'];
+  }
+  ;
+
+  var data = [
+    [button_oneLabel, button_oneList],
+    [button_twoLabel, button_twoList],
+    [button_threeLabel, button_threeList],
+  ];
+  return data;
+}
+
+Future<void> _showMyDialog(context) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        scrollable: true,
+        title: const Text('AlertDialog Title'),
+        content: const SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Row(children: [Text('Button One: Name'), TextField()]),
+              Text('This is a demo alert dialog.'),
+              Text('Would you like to approve of this message?'),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('Approve'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
